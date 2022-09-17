55c6bcd5293f24947e0561eb10a2b0b6cd698a6c
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-09-17 22:50:05 +0100

    testing if it showcases the right err msg

--- cypress/e2e/reward-app-tests/accomplisments.cy.js
+++ cypress/e2e/reward-app-tests/accomplisments.cy.js
@@ -4,4 +4,15 @@ describe('Accomplishment dashboard', () => {
   beforeEach(() => {
     cy.visit('/accomplishments');
   });
+
+  it('should showcase error if information is missing', () => {
+    cy.get("[data-cy='accomplishment-title-input']").type(
+      'My Basketball Accomplishment'
+    );
+    cy.get("[data-cy='accomplishment-input']").type(
+      'I made 10 threes in a row'
+    );
+    cy.contains('Submit Accomplishment').click();
+    cy.contains('Complete the items above to continue').should('be.visible');
+  });
 });
