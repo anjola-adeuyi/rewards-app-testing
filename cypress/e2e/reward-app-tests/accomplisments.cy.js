135272be457018dd1ebc3d44fff249cf666a125b
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-09-17 22:50:35 +0100

    validation animated & displayed

--- cypress/e2e/reward-app-tests/accomplisments.cy.js
+++ cypress/e2e/reward-app-tests/accomplisments.cy.js
@@ -15,4 +15,18 @@ describe('Accomplishment dashboard', () => {
     cy.contains('Submit Accomplishment').click();
     cy.contains('Complete the items above to continue').should('be.visible');
   });
+
+  it('should display validation component if all information is input', () => {
+    cy.get("[data-cy='accomplishment-title-input']").type(
+      'My Basketball Accomplishment'
+    );
+    cy.get("[data-cy='accomplishment-input']").type(
+      'I made 10 threes in a row'
+    );
+    cy.get("[type='checkbox']").click();
+    cy.contains('Submit Accomplishment').click();
+    cy.contains('This Accomplisment was Successfully Submitted').should(
+      'be.visible'
+    );
+  });
 });
