f24b2631c9decb218620ebe8e930e5fb1d0dac4a
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-09-17 22:51:27 +0100

    PROPERLY RETURN BACK TO DASHBOARD WITH EMPTY INPUT

--- cypress/e2e/reward-app-tests/accomplisments.cy.js
+++ cypress/e2e/reward-app-tests/accomplisments.cy.js
@@ -29,4 +29,19 @@ describe('Accomplishment dashboard', () => {
       'be.visible'
     );
   });
+
+  it("should return back to accomplishment dashboard with empty inputs when 'Go Back' button is clicked", () => {
+    cy.get("[data-cy='accomplishment-title-input']").type(
+      'My Basketball Accomplishment'
+    );
+    cy.get("[data-cy='accomplishment-input']").type(
+      'I made 10 threes in a row'
+    );
+    cy.get("[type='checkbox']").click();
+    cy.contains('Submit Accomplishment').click();
+    cy.contains('Go Back').click();
+    cy.get("[data-cy='accomplishment-title-input']").should('have.value', '');
+    cy.get("[data-cy='accomplishment-input']").should('have.value', '');
+    cy.get("[type='checkbox']").should('not.be.checked');
+  });
 });
