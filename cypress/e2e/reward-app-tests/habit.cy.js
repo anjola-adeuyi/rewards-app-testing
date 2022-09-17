637474a03e00a52d5eb9dcebf94c964e25e62058
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-09-17 22:35:06 +0100

    new test case instance

--- cypress/e2e/reward-app-tests/habit.cy.js
+++ cypress/e2e/reward-app-tests/habit.cy.js
@@ -18,4 +18,14 @@ describe('habit dashboard', () => {
       .should('be.visible')
       .and('have.class', 'HabitCard__habit-container');
   });
+
+  it('should toggle icon when habit card is clicked', () => {
+    cy.contains('button', 'Add').click();
+    cy.get("input[placeholder='Habit']").type('Drink a cup of water');
+    cy.contains('Save Changes').click();
+    cy.contains('Drink a cup of water').click();
+    cy.get("[src='/static/media/check.9e8832df.svg']").should('be.visible');
+    cy.contains('Drink a cup of water').click();
+    cy.get("[src='/static/media/close.fa7e5ead.svg']").should('be.visible');
+  });
 });
