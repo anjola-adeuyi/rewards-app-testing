6a2e5a12e2ba6ac7b7d827cadc9460dc06817b69
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-09-17 22:34:27 +0100

    it* displays habit card when habit added

--- cypress/e2e/reward-app-tests/habit.cy.js
+++ cypress/e2e/reward-app-tests/habit.cy.js
@@ -9,4 +9,13 @@ describe('habit dashboard', () => {
     cy.contains('button', 'Add').click();
     cy.contains('Add a new habit').should('be.visible');
   });
+
+  it('should display habit card when new habit is added', () => {
+    cy.contains('button', 'Add').click();
+    cy.get("input[placeholder='Habit']").type('Drink a cup of water');
+    cy.contains('Save Changes').click();
+    cy.contains('Drink a cup of water')
+      .should('be.visible')
+      .and('have.class', 'HabitCard__habit-container');
+  });
 });
